0e8cbd986506dd42689f2fe6ba90a4372b0152ed
HemanthMaram <83005267+HemanthMaram@users.noreply.github.com>
2021-08-08 00:47:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,9 +29,9 @@ document.body.append(div1);
 async function data() {
   var moviename = document.getElementById("title-moviedata").value;
   console.log(moviename)
-   if(moviename === ""){
+  /* if(moviename === ""){
       alert('Enter the movie name!')
-     }
+     }*/
    
 
   let res = await fetch(url + moviename);
